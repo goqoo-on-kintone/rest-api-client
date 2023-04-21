a1de0f53dabcb95659faa47a31bcdd5b917aa000
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-04-21 11:09:18 +0900

    feat: @kintone/rest-api-clientをそのままパッケージ

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -1,1 +1,7 @@
-const bar: number = 1
+import { injectPlatformDeps } from '../node_modules/@kintone/rest-api-client/esm/platform/'
+import * as browserDeps from '../node_modules/@kintone/rest-api-client/esm/platform/browser'
+
+injectPlatformDeps(browserDeps)
+
+export { KintoneRestAPIClient } from '../node_modules/@kintone/rest-api-client/esm/KintoneRestAPIClient'
+export * from '../node_modules/@kintone/rest-api-client/esm/error'
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-const foo: number = 1
+import { injectPlatformDeps } from '../node_modules/@kintone/rest-api-client/esm/platform/'
+import * as nodeDeps from '../node_modules/@kintone/rest-api-client/esm/platform/node'
+
+injectPlatformDeps(nodeDeps)
+
+export { KintoneRestAPIClient } from '../node_modules/@kintone/rest-api-client/esm/KintoneRestAPIClient'
+export * from '../node_modules/@kintone/rest-api-client/esm/error'
+export * as KintoneRecordField from '../node_modules/@kintone/rest-api-client/esm/KintoneFields/exportTypes/field'
+export * as KintoneFormLayout from '../node_modules/@kintone/rest-api-client/esm/KintoneFields/exportTypes/layout'
+export * as KintoneFormFieldProperty from '../node_modules/@kintone/rest-api-client/esm/KintoneFields/exportTypes/property'
