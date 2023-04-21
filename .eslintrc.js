7c4dd3f8ddccbfe803e683c44afd5b175df66728
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-04-21 10:51:03 +0900

    chore: ビルド環境が動くように

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: '@cybozu/eslint-config/presets/node-typescript-prettier',
+  rules: {
+    'curly': ['error', 'all'],
+    'func-style': ['error'],
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          'object': false,
+          '{}': false,
+        },
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+      },
+    ],
+  },
+}
---
+++ rollup.config.mjs
@@ -0,0 +1,107 @@
+/* eslint-disable node/no-unpublished-import */
+import { defineConfig } from 'rollup'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import { terser } from 'rollup-plugin-terser'
+import replace from 'rollup-plugin-replace'
+import license from 'rollup-plugin-license'
+
+// TODO: After importing JSON module become stable, we can import package.json as follows
+// JSON module: https://github.com/tc39/proposal-json-modules
+// Import Assertions: https://github.com/tc39/proposal-import-assertions
+// import pkgJson from "./package.json" assert { type: "json" };
+import { createRequire } from 'module'
+const pkgJson = createRequire(import.meta.url)('./package.json')
+
+import builtins from 'rollup-plugin-node-builtins'
+import globals from 'rollup-plugin-node-globals'
+import babel from '@rollup/plugin-babel'
+import { ecmaVersionValidator } from 'rollup-plugin-ecma-version-validator'
+import fs from 'fs'
+
+const licenseText = fs.readFileSync('LICENSE', 'utf-8')
+const licenseTemplate = `
+${licenseText}
+
+This bundle includes the following third-party libraries:
+<% _.forEach(dependencies, function (dependency) { %>
+  =====
+  <%= dependency.name %>@<%= dependency.version %> -- <%= dependency.license %>
+  -----
+
+  <%= dependency.licenseText %>
+<% }) %>
+`
+
+const extensions = ['.ts', '.js']
+
+const isProd = process.env.BUILD === 'production'
+
+export default defineConfig({
+  input: './src/index.browser.ts',
+  output: {
+    extend: true,
+    file: `./umd/KintoneRestAPIClient${isProd ? '.min' : ''}.js`,
+    format: 'umd',
+    name: 'window',
+    sourcemap: isProd ? false : 'inline',
+  },
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            corejs: 3,
+            useBuiltIns: 'usage',
+          },
+        ],
+        '@babel/preset-typescript',
+      ],
+      extensions,
+      include: ['src/**/*'],
+    }),
+    resolve({
+      browser: true,
+      preferBuiltins: false,
+    }),
+    commonjs({ extensions }),
+    babel({
+      babelHelpers: 'bundled',
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              // see kintone's supported browsers https://get.kintone.help/general/en/user/list_start/webbrowser.html
+              browsers: [
+                'last 2 edge versions',
+                'last 2 firefox version',
+                'last 2 chrome versions',
+                'last 2 safari versions',
+                'iOS >= 14',
+                'last 2 and_chr versions',
+              ],
+            },
+          },
+        ],
+      ],
+    }),
+    json(),
+    replace({
+      PACKAGE_VERSION: JSON.stringify(pkgJson.version),
+    }),
+    globals(),
+    builtins(),
+    isProd && terser(),
+    ecmaVersionValidator({ ecmaVersion: 6 }),
+    license({
+      banner: {
+        commentStyle: 'regular',
+        content: licenseTemplate,
+      },
+    }),
+  ],
+})
