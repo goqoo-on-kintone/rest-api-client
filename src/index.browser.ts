bcdd0607dd724b37125f547d6781959e245c3f0f
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-04-21 11:20:47 +0900

    fix: 普通のimportに修正

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -1,7 +1,7 @@
-import { injectPlatformDeps } from '../node_modules/@kintone/rest-api-client/esm/platform/'
-import * as browserDeps from '../node_modules/@kintone/rest-api-client/esm/platform/browser'
+import { injectPlatformDeps } from '@kintone/rest-api-client/esm/platform/'
+import * as browserDeps from '@kintone/rest-api-client/esm/platform/browser'
 
 injectPlatformDeps(browserDeps)
 
-export { KintoneRestAPIClient } from '../node_modules/@kintone/rest-api-client/esm/KintoneRestAPIClient'
-export * from '../node_modules/@kintone/rest-api-client/esm/error'
+export { KintoneRestAPIClient } from '@kintone/rest-api-client/esm/KintoneRestAPIClient'
+export * from '@kintone/rest-api-client/esm/error'
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { injectPlatformDeps } from '../node_modules/@kintone/rest-api-client/esm/platform/'
-import * as nodeDeps from '../node_modules/@kintone/rest-api-client/esm/platform/node'
+import { injectPlatformDeps } from '@kintone/rest-api-client/esm/platform/'
+import * as nodeDeps from '@kintone/rest-api-client/esm/platform/node'
 
 injectPlatformDeps(nodeDeps)
 
-export { KintoneRestAPIClient } from '../node_modules/@kintone/rest-api-client/esm/KintoneRestAPIClient'
-export * from '../node_modules/@kintone/rest-api-client/esm/error'
-export * as KintoneRecordField from '../node_modules/@kintone/rest-api-client/esm/KintoneFields/exportTypes/field'
-export * as KintoneFormLayout from '../node_modules/@kintone/rest-api-client/esm/KintoneFields/exportTypes/layout'
-export * as KintoneFormFieldProperty from '../node_modules/@kintone/rest-api-client/esm/KintoneFields/exportTypes/property'
+export { KintoneRestAPIClient } from '@kintone/rest-api-client/esm/KintoneRestAPIClient'
+export * from '@kintone/rest-api-client/esm/error'
+export * as KintoneRecordField from '@kintone/rest-api-client/esm/KintoneFields/exportTypes/field'
+export * as KintoneFormLayout from '@kintone/rest-api-client/esm/KintoneFields/exportTypes/layout'
+export * as KintoneFormFieldProperty from '@kintone/rest-api-client/esm/KintoneFields/exportTypes/property'
